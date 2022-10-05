a8672606e6d1976955b5e723ccf5a643461aa9f4
songmiO <dhthd1060@gmail.com>
2022-10-05 18:34:09 +0900

    Add : Add Link tag

--- 01/index.js
+++ 01/index.js
@@ -3,3 +3,9 @@ document
   .addEventListener('click', function () {
     document.getElementsByClassName('color-code')[0].innerHTML = '#2BC10B';
   });
+
+document
+  .getElementsByClassName('change-btn')[0]
+  .addEventListener('click', function () {
+    document.getElementsByClassName('color-code')[0].innerHTML = '#73989E';
+  });
