89135fb280bdbacd0879a48fbe73b0455bc63d20
songmiO <dhthd1060@gmail.com>
2022-10-06 22:03:00 +0900

    Remove : Remove Click event code

--- 01/index.js
+++ 01/index.js
@@ -3,9 +3,3 @@ document
   .addEventListener('click', function () {
     document.getElementsByClassName('color-code')[0].innerHTML = '#2BC10B';
   });
-
-document
-  .getElementsByClassName('change-btn')[0]
-  .addEventListener('click', function () {
-    document.getElementsByClassName('color-code')[0].innerHTML = '#73989E';
-  });
