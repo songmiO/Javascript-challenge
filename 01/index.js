36b63f5b484ecdff847749ec4b0676422c735c44
songmiO <dhthd1060@gmail.com>
2022-10-13 20:55:56 +0900

    Add : Add console

--- 01/index.js
+++ 01/index.js
@@ -1,5 +1,6 @@
 document
   .getElementsByClassName('change-btn')[0]
   .addEventListener('click', function () {
+    console.log('확인');
     document.getElementsByClassName('color-code')[0].innerHTML = '#2BC10B';
   });
