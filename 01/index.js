abb87958137ca9853c151326371c96971328c547
songmiO <dhthd1060@gmail.com>
2022-10-16 22:08:13 +0900

    Style : Modify color code

--- 01/index.js
+++ 01/index.js
@@ -1,5 +1,5 @@
 document
   .getElementsByClassName('change-btn')[0]
   .addEventListener('click', function () {
-    document.getElementsByClassName('color-code')[0].innerHTML = '#2BC10B';
+    document.getElementsByClassName('color-code')[0].innerHTML = '#FF9A00';
   });
