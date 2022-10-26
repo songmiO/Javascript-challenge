1c141d1f052a8b650465b35c9ef924c476d7715d
songmiO <dhthd1060@gmail.com>
2022-10-26 18:21:05 +0900

    Add : On button click, Counter number decrease and increase

--- 02/index.js
+++ 02/index.js
@@ -1,1 +1,35 @@
+const minusBtn = document.getElementsByClassName('decrease-btn')[0];
+const resetBtn = document.getElementsByClassName('reset-btn')[0];
+const plusBtn = document.getElementsByClassName('increase-btn')[0];
+// 카운트 할 숫자
+let countNum = 0;
 
+// 버튼 클릭 시, 하나씩 감소하는 함수
+minusBtn.addEventListener('click', function () {
+  const changeNum = document.getElementsByClassName('counter-num')[0];
+
+  countNum -= 1;
+
+  changeNum.innerHTML = countNum;
+  changeNum.style.color = 'red';
+});
+
+// 버튼 클릭 시, 0으로 리셋 시키는 함수
+resetBtn.addEventListener('click', function () {
+  const changeNum = document.getElementsByClassName('counter-num')[0];
+
+  countNum = 0;
+
+  changeNum.innerHTML = countNum;
+  changeNum.style.color = 'black';
+});
+
+// 버튼 클릭 시, 하나씩 증가하는 함수
+plusBtn.addEventListener('click', function () {
+  const changeNum = document.getElementsByClassName('counter-num')[0];
+
+  countNum += 1;
+
+  changeNum.innerHTML = countNum;
+  changeNum.style.color = 'green';
+});
