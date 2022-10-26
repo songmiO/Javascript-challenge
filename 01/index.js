7a051b4b0213b84fe52f0b446807067fc4fc48f3
songmiO <dhthd1060@gmail.com>
2022-10-26 16:42:21 +0900

    Add : Add background color and background color random play code

--- 01/index.js
+++ 01/index.js
@@ -7,8 +7,18 @@ const colors = [
   '#F1f5f8',
 ];
 const changeBtn = document.getElementsByClassName('change-btn')[0];
-const colorCode = document.getElementsByClassName('color-code')[0];
+// const colorCode = document.getElementsByClassName('color-code')[0];
 
+// 버튼 클릭 할 때마다 colors 색상 랜덤 변경
 changeBtn.addEventListener('click', function () {
-  // Math.floor(Math.random() * colorCode.length);
+  const code = Math.floor(Math.random() * colors.length);
+  const display = document.getElementsByClassName('main-container')[0];
+  display.style.backgroundColor = colors[code];
+});
+
+// 버튼 클릭 할 때마다 color code 랜덤 변경
+changeBtn.addEventListener('click', function () {
+  const number = Math.floor(Math.random() * colors.length);
+  const codeNumber = document.getElementsByClassName('color-code')[0];
+  codeNumber.innerHTML = colors[number];
 });
